75b1213f17c55a5e0c7ee1783947cc91018ee0c6
Alireza <alireza.kariminejad@yahoo.com>
2022-11-18 18:53:41 +0330

    Create Simple CV Project

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp();
+}
+
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
