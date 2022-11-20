5aa45814b9e601d9faa2b5a5f7a97fdf846df75e
Alireza <alireza.kariminejad@yahoo.com>
2022-11-21 00:36:29 +0330

    Remove Debag Banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: 'Vazir'),
       home: Scaffold(
         backgroundColor: Colors.amber,
